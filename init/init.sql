a6d222917e49a5963e92288ba514130df192f738
ABDENOUR TEGGOUR <abdenour.teggour@richemont.com>
2024-12-23 11:17:48 +0100

    feat add init.sql

---
+++ init/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `springbootadvertissement`;
+CREATE DATABASE IF NOT EXISTS `springbootreport`;
+CREATE DATABASE IF NOT EXISTS `springbootuser`;
+
+-- CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
+-- GRANT ALL ON *.* TO 'root'@'%';
+-- GRANT ALL PRIVILEGES ON `springbootadvertissement` TO 'springmicroserviceuser'@'%' WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON `springbootreport` TO 'springmicroserviceuser'@'%' WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON `springbootuser` TO 'springmicroserviceuser'@'%' WITH GRANT OPTION;
+
+GRANT ALL ON `springbootadvertisement`.* TO 'springmicroserviceuser'@'%';
+GRANT ALL ON `springbootreport`.* TO 'springmicroserviceuser'@'%';
+GRANT ALL ON `springbootuser`.* TO 'springmicroserviceuser'@'%';
